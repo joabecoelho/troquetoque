9e74a679a3dd0bb72b977b0e6e328e9af170b118
joabecoelho <contatojoabe@icloud.com>
2023-06-06 12:25:32 -0300

    Audios troll

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -12,25 +12,24 @@ function Navbar() {
   const audioRef = useRef(null)
   const { authenticated, logout } = useContext(Context)
 
-  function handleClick() {
+  function handleClick(e) {
+    e.preventDefault(); // Prevent the default navigation behavior
     audioRef.current.src = "./audio/zedamanga.mp3"
     audioRef.current.play()
   }
-
-  audioRef.current.play()
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_content}>
         <div className={styles.navbar_logo}>
-          <a href='/'>
+          <a onClick={handleClick} href='/'>
             <img src={Logo} alt='Troque e Toque'/>
             <h2>Troque e Toque</h2>
           </a>
         </div>
         <ul>
           <li>
-            <Link to="/" onClick={handleClick}>Trocar</Link>
+            <Link to="/">Trocar</Link>
           </li>
           {authenticated ? (
             <>
